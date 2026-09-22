b8558aa99b95e00b49c2cd607dc1200b15b47c4d
Leidy Bravo <leidymaireth5555@gmail.com>
2026-09-22 17:43:12 -0500

    Agregar HashMap para controlar codigos de libros

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Biblioteca {
 
     private ArrayList<Libro> libros;
+    private HashMap<String, Libro> librosPorCodigo;
 
     public Biblioteca() {
-
         libros = new ArrayList<>();
+        librosPorCodigo = new HashMap<>();
     }
 
     public boolean agregarLibro(Libro libro) {
 
-        return libros.add(libro);
+        if (librosPorCodigo.containsKey(libro.getCodigo())) {
+            return false;
+        }
+
+        libros.add(libro);
+        librosPorCodigo.put(libro.getCodigo(), libro);
+
+        return true;
     }
 
     public void eliminarLibro(Libro libro) {
-
         libros.remove(libro);
-
+        librosPorCodigo.remove(libro.getCodigo());
     }
 
     public ArrayList<Libro> obtenerTodos() {
@@ -26,6 +34,15 @@ public class Biblioteca {
 
     public ArrayList<Libro> filtrarPorAutor(String autor) {
 
-        return libros;
+        ArrayList<Libro> resultado = new ArrayList<>();
+
+        for (Libro libro : libros) {
+
+            if (libro.getAutor().equals(autor)) {
+                resultado.add(libro);
+            }
+        }
+
+        return resultado;
     }
 }
