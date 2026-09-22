161c4dbc13239f171a921bad3991114c2eba1e9f
Leidy Bravo <leidymaireth5555@gmail.com>
2026-09-22 18:10:46 -0500

    Inicializar Biblioteca y VentanaPrincipal

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Biblioteca biblioteca = new Biblioteca();
+
+        VentanaPrincipal ventana = new VentanaPrincipal(biblioteca);
+
+        ventana.setVisible(true);
+
+    }
 }
