bdd35b1ebbddbb76a36c96c30c18b8a3720499a0
Leidy Bravo <leidymaireth5555@gmail.com>
2026-09-22 17:25:11 -0500

    Se añadio a la clase Libro(atributos y métodos)

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,50 @@
+
 public class Libro {
+
+        private String titulo;
+        private String autor;
+        private String codigo;
+        private String genero;
+        private int añoPublicacion;
+        private int copiasDisponibles;
+
+        public Libro(String titulo, String autor, String codigo, String genero, int añoPublicacion, int copiasDisponibles) {
+
+            this.titulo = titulo;
+            this.autor = autor;
+            this.codigo = codigo;
+            this.genero = genero;
+            this.añoPublicacion = añoPublicacion;
+            this.copiasDisponibles = copiasDisponibles;
+
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+
+        public void setAutor(String autor) {
+            this.autor = autor;
+        }
+
+        public String getCodigo() {
+            return codigo;
+        }
+
+        public void setCodigo(String codigo) {
+            this.codigo = codigo;
+        }
+
+        public String getGenero() {
+            return genero;
+        }
+
 }
