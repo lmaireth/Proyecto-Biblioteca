f7a8281226ea03e34b3247e4e43a4896a0a420b3
Leidy Bravo <leidymaireth5555@gmail.com>
2026-09-22 18:08:40 -0500

    Se crea la estructura base de la ventana principal

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,2 +1,16 @@
-public class VentanaPrincipal {
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+    private Biblioteca biblioteca;
+
+    public VentanaPrincipal(Biblioteca biblioteca) {
+
+        this.biblioteca = biblioteca;
+
+        setTitle("Biblioteca");
+        setSize(800, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
