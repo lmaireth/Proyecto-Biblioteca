7cd8c7244534631842dbb9e0d1c8d2f440bb77fd
Leidy Bravo <leidymaireth5555@gmail.com>
2026-09-22 17:33:12 -0500

    Se añadio a la clase Biblioteca los metodos

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+    private ArrayList<Libro> libros;
+
+    public Biblioteca() {
+
+        libros = new ArrayList<>();
+    }
+
+    public boolean agregarLibro(Libro libro) {
+
+        return libros.add(libro);
+    }
+
+    public void eliminarLibro(Libro libro) {
+
+        libros.remove(libro);
+
+    }
+
+    public ArrayList<Libro> obtenerTodos() {
+        return libros;
+    }
+
+    public ArrayList<Libro> filtrarPorAutor(String autor) {
+
+        return libros;
+    }
 }
